0aa7d9fb4e0e2474f78492d6cac46442700b6aed
percussion1313 <peterjacobgrant@gmail.com>
2018-06-04 12:46:07 -0500

    Added function setName

--- Advanced JS Drills/app.js
+++ Advanced JS Drills/app.js
@@ -3,4 +3,12 @@ console.log('Helllllooooo Jerry');
 name = 'Peter';
 var name;
 console.log(name);
-// I expect an undefined log
+// I expect an undefined log. I was wrong. Should have seen that coming
+
+setName();
+function setName() {
+    var name = 'Peter';
+    console.log(name);
+};
+
+//I expect my name to show up through hoisting 
